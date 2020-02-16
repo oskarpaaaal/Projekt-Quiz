73bb25763d9543158bc687665e3f08d007b38033
oskarpaaaal <57439618+oskarpaaaal@users.noreply.github.com>
2020-02-16 13:48:08 +0100

    Add files via upload

--- skript.js
+++ skript.js
@@ -4,6 +4,12 @@ function check(){
 	var fråga3 = document.quiz.fråga3.value;
 	var fråga4 = document.quiz.fråga4.value;
 	var fråga5 = document.quiz.fråga5.value;
+	var fråga6 = document.quiz.fråga6.value;
+	var fråga7 = document.quiz.fråga7.value;
+	var fråga8 = document.quiz.fråga8.value;
+	var fråga9 = document.quiz.fråga9.value;
+	var fråga10 = document.quiz.fråga10.value;
+
 	var correct = 0;
 
 	if (fråga1 == "354") {
@@ -22,20 +28,37 @@ function check(){
 		correct++;
 	}
 
+	if (fråga6 == "1953") {
+		correct++;
+	}
+	if (fråga7 == "Silver") {
+		correct++;
+	}
+	if (fråga8 == "Finland") {
+		correct++;
+	}
+	if (fråga9 == "Sedin") {
+		correct++;
+	}
+	if (fråga10 == "Schweiz") {
+		correct++;
+	}
+
+
 	var messages = ["Bra jobbat!", "Det är godkänt..", "Du måste bli bättre"];
 
 	var range;
 
-		if (correct < 2) {
+		if (correct < 8) {
 			range = 2;
 		}
 
-		if ( correct < 5 ) {
+		if ( correct < 8 ) {
 			range = 1;
 		}
 
 
-		if (correct < 1) {
+		if (correct < 4) {
 			range = 0;
 		}
 		
